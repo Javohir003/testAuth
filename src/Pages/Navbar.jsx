e166e1dc69c2424f5797c12ccac2a1b80184affd
Javohir <yusupovjavoxir11@gmail.com>
2024-06-26 18:26:29 +0500

    new message Auth0

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Style/Navbar.css";
 import { Link, Outlet } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
@@ -7,6 +7,11 @@ function Navbar() {
   const { loginWithRedirect, user, isAuthenticated, isLoading, logout } =
     useAuth0();
   const { picture, email, name } = useAuth0();
+  const [active, setActive] = useState(false);
+
+  const handleChangeActive = () => {
+    setActive(!active);
+  };
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -28,17 +33,31 @@ function Navbar() {
             {!isLoading && !user && (
               <Link onClick={() => loginWithRedirect()}>Login</Link>
             )}
+
             {!isLoading && user && (
               <button onClick={() => logout()}>Log Out</button>
             )}
             {isAuthenticated && user && (
-              <article className="avatar">
+              <article className="avatar" onClick={handleChangeActive}>
                 <img src={user.picture} alt="" />
-                <h2>{user.name}</h2>
+                {/* <h2>{user.name}</h2> */}
               </article>
             )}
           </ul>
         </nav>
+        {/* <div className={`porfile-wrapper ${active ? "active" : ""}`}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <h2>{user.name}</h2>
+            <h4>{user.email}</h4>
+          </div>
+        </div> */}
       </header>
       <main>
         <Outlet />
